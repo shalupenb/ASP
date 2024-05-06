a380dbf5cec37cf0743bc3d9f23379bc5c7382db
Danya Gichak <danyacorporation6@gmail.com>
2024-05-06 11:20:54 +0300

    commit

---
+++ ASP/Controllers/NotifyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Controllers
+{
+	[Route("api/notify")]
+	[ApiController]
+	public class NotifyController : ControllerBase
+	{
+
+	}
+}
